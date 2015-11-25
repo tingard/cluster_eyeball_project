a27279472ca81e4ee58aa5e136e30cf5fc47b0fe
tingard <tklingard@gmail.com>
2015-11-25 13:34:07 +0000

    messin wiv php

--- flag_handler.php
+++ flag_handler.php
@@ -1,8 +1,16 @@
 <?php 
     $index = $_POST['index'];
     $flag = $_POST['flag'];
-    $file = 'received_changes.csv';
-    $current = file_get_contents($file);
+    $file = 'bin/received_changes.csv';
+    if ( !file_exists('bin') ) {
+        $oldmask = umask(0);
+        mkdir('bin', 0744);
+    }
+    if ( file_exists($file) ) {
+        $current = file_get_contents($file);
+    } else {
+        $current = "";
+    }
     $current .= $index . ",". $flag ."\n";
     echo $current;
     file_put_contents($file, $current) or die('write failed'); 
