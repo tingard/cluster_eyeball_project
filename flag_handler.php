1f804c0a9a51c88445c342ddb5a5dbb66901e071
tingard <tklingard@gmail.com>
2015-11-25 13:24:47 +0000

    :D

--- flag_handler.php
+++ flag_handler.php
@@ -1,4 +1,9 @@
 <?php 
-    echo $_POST['index']."  ";
-    echo $_POST['flag'];
+    $index = $_POST['index']."  ";
+    $flag = $_POST['flag'];
+    $file = 'recieved_changes.csv';
+    $current = file_get_contents($file);
+    $current .= $index . ",". $flag ."\n";
+    file_put_contents($file, $current);
+    echo "Appended ". $index . ",". $flag . " to csv";
 ?>
