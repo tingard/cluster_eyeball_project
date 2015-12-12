bc3f57078879b0aead8b225695c22e39e6af6441
tingard <tklingard@gmail.com>
2015-12-12 12:02:57 +0000

    :D

--- flag_handler.php
+++ flag_handler.php
@@ -1,13 +1,14 @@
 <?php 
     $index = $_POST['index'];
     $flag = $_POST['flag'];
+    $name = $_POST['name'];
     $file = './php_bin/received_changed.csv';
     if ( file_exists($file) ) {
         $current = file_get_contents($file);
     } else {
         $current = "";
     }
-    $current .= $index . ",". $flag ."\n";
+    $current .= $index . ",". $flag . "," . $name . "\n";
     //echo $index . "  " . $flag;
     file_put_contents($file, $current) or die('write failed'); 
     echo "Changes Added to approval list";
